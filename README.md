69a84ce8b5a91807f5f4a402996abd8ba8e55a85
giorgenes <giorgenes@gmail.com>
2021-08-28 06:07:31 +0000

    Relation search wip

--- README.md
+++ README.md
@@ -3,19 +3,10 @@
 [![CircleCI](https://circleci.com/gh/circleci/circleci-docs.svg?style=svg)](https://circleci.com/gh/circleci/circleci-docs)
 
 
-## The ultimate ruby interview booster
+## Caveats
 
-Use this to speed up your small interview tasks
-by quickly setting up guard and test:unit
-to test your interview problems.
+- I'm not handling data types. Types are converted to string for simplicity to avoid having to figure out the data
+  and map between user input and the database. Also to prevent having different types of indexes.
+- When displaying related data, I'm searching for a hardcoded field `name` to avoid extra configuration of what field to display.
 
-You can use this in contrast to traditional stdin/stdout
-testing.
 
-## Usage
-
-  $> bundle install
-  $> bundle exec guard
-
-Then edit example.rb and test/example_test.rb and
-create your solution/tests respectively.
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,106 +1,133 @@
 class CLI
-    class UserQuit < RuntimeError
+  class UserQuit < RuntimeError
+  end
+
+  def initialize(input: STDIN, output: STDOUT, databases: nil, relations: nil)
+    @in = input
+    @out = output
+    @databases = databases
+    @relations = relations
+  end
+
+  def execute
+    show_greeting
+
+    begin
+      loop do
+        cmd = show_prompt("> ", %w[1 2])
+
+        handle_cmd(cmd)
+      end
+    rescue UserQuit => e
     end
-    
-    def initialize(input: STDIN, output: STDOUT, databases: nil)
-        @in = input
-        @out = output
-        @databases = databases
-    end
-    
-    def execute
-        show_greeting
-        
-        begin
-            loop do
-                cmd = show_prompt('> ', %w[1 2])
-                
-                handle_cmd(cmd)
-            end
-        rescue UserQuit => e
+
+    @out.puts "Goodbye!"
+  end
+
+  private
+
+  def handle_cmd(cmd)
+    case cmd
+    when "1"
+      db = db_prompt
+      term = show_prompt("Enter search term: ", %w[_id owner_id tags])
+      value = show_prompt("Enter search value: ", nil)
+      @out.puts "Searching users for #{term} with a value of #{value}"
+
+      docs = db.find_by(term, value)
+
+      if docs.empty?
+        @out.puts "No documents found"
+      else
+        docs.each do |doc|
+          print_doc(db, doc)
         end
-        
-        @out.puts "Goodbye!"
+      end
+    when "2"
+      @databases.each do |db|
+        @out.puts "Search #{db.name} with: #{db.fields.join(", ")}"
+      end
     end
-    
-    private
-    
-    def handle_cmd(cmd)
-        case cmd
-        when '1'
-            db = db_prompt
-            term = show_prompt("Enter search term: ", %w[_id owner_id tags])
-            value = show_prompt("Enter search value: ", nil)
-            @out.puts "Searching users for #{term} with a value of #{value}"
-            
-            docs = db.find_by(term, value)
-            
-            if docs.empty?
-                @out.puts 'No documents found'
-            else
-                docs.each do |doc|
-                    print_doc(doc)
-                end
-            end
-        when '2'
-            @databases.each do |db|
-                @out.puts "Search #{db.name} with: #{db.fields.join(', ')}"
-            end
+  end
+
+  def db_by_name(name)
+    @databases.find { |db| db.name == name }
+  end
+
+  def find_and_print_related(db, doc)
+    rel = @relations[db.name]
+    return unless rel
+
+    key = rel.keys.first
+    return unless key
+
+    dbname, field = rel.values.first.split(".")
+    return unless dbname && field
+
+    related_db = db_by_name(dbname)
+    return unless related_db
+
+    related_docs = related_db.find_by(field, doc[key])
+
+    if related_docs.any?
+      @out.puts "#{related_db}: #{related_docs.map { |rd| rd["name"] }.join(", ")}"
+    end
+  end
+
+  def print_doc(db, doc)
+    max_key_size = doc.keys.map(&:size).max
+    doc.each_pair do |k, v|
+      @out.puts("%-*s = #{v}" % [max_key_size, k])
+    end
+
+    find_and_print_related(db, doc)
+  end
+
+  def show_greeting
+    @out.puts "Welcome to Zendesk search"
+    @out.puts "Type 'quit' to exit at any time. Press 'ENTER' to continue"
+    @out.puts
+    @out.puts "\tSelect search options:"
+    @out.puts "\t\t* Press 1 to search Zendesk"
+    @out.puts "\t\t* Press 2 to view a list of searcheable fields"
+    @out.puts "\t\t* Type 'quit' to exist"
+    @out.puts
+  end
+
+  # options = list of accepted answers or nil for any
+  def show_prompt(prompt, options)
+    begin
+      loop do
+        @out.print prompt
+        cmd = @in.readline.strip
+
+        if options.nil? || options.include?(cmd)
+          return cmd
         end
-    end
-    
-    def print_doc(doc)
-        max_key_size = doc.keys.map(&:size).max
-        doc.each_pair do |k,v|
-            @out.puts("%-*s = #{v}" % [max_key_size, k])
+
+        if cmd == "quit"
+          raise UserQuit.new
         end
+
+        @out.puts "Invalid command"
+      end
+    rescue EOFError
+      # You've reached the end. Handle it.
     end
-    
-    def show_greeting
-        @out.puts "Welcome to Zendesk search"
-        @out.puts "Type 'quit' to exit at any time. Press 'ENTER' to continue"
-        @out.puts
-        @out.puts "\tSelect search options:"
-        @out.puts "\t\t* Press 1 to search Zendesk"
-        @out.puts "\t\t* Press 2 to view a list of searcheable fields"
-        @out.puts "\t\t* Type 'quit' to exist"
-        @out.puts
-    end
-    
-    # options = list of accepted answers or nil for any
-    def show_prompt(prompt, options)
-        begin
-            loop do
-                @out.print prompt
-                cmd = @in.readline.strip
-                
-                if options.nil? || options.include?(cmd)
-                    return cmd
-                end
-                
-                if cmd == 'quit'
-                    raise UserQuit.new
-                end
-                
-                @out.puts "Invalid command"
-            end
-        rescue EOFError
-            # You've reached the end. Handle it.
-        end
-        
-        raise UserQuit.new
-    end
-    
-    def db_options
-        (1..@databases.size).map(&:to_s)
-    end
-    
-    def db_prompt
-        options = (1..@databases.size).map(&:to_s)
-        prompt = @databases.each_with_index.map{ |db, index| "#{index+1}) #{db.name}" }.join(" or ")
-        
-        op = show_prompt("Select #{prompt}: ", options)
-        
-        @databases[op.to_i - 1]
-    end
+
+    raise UserQuit.new
+  end
+
+  def db_options
+    (1..@databases.size).map(&:to_s)
+  end
+
+  def db_prompt
+    options = (1..@databases.size).map(&:to_s)
+    prompt = @databases.each_with_index.map { |db, index| "#{index + 1}) #{db.name}" }.join(" or ")
+
+    op = show_prompt("Select #{prompt}: ", options)
+
+    @databases[op.to_i - 1]
+  end
 end
